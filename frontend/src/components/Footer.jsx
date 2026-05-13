89e79c3901fd3c127600879519b63efe0919b0e7
Swarup Salunke <swarupsalunke01@gmail.com>
2026-05-13 18:39:38 +0530

    fix rename

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -80,7 +80,7 @@ export default function Footer() {
                                 Become Exhibitor
                             </Link>
 
-                            <Link to="/delegate" className="footer-link">
+                            <Link to="/Delegate" className="footer-link">
                                 <svg width="14" height="14" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
                                     <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
                                     <circle cx="9" cy="7" r="4" />
